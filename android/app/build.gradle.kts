3d81d7b9b1396326d99af280eef51bb4ebc6ad6b
santhoshkanna2506 <santhoshkannap135@gmail.com>
2026-05-06 18:58:11 +0530

    added firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.app.pack_n_pay"
+    namespace = "com.app.packnpay"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.app.pack_n_pay"
+        applicationId = "com.app.packnpay"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -34,7 +35,7 @@ android {
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
